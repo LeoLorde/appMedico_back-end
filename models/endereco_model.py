4d0b04c6451e51ecb6759c9602c5afb03705a823
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-15 09:15:27 -0300

    feat: creating endereco model

---
+++ models/endereco_model.py
@@ -0,0 +1,18 @@
+from app import db
+
+class Endereco(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cidade = db.Column(db.String, nullable=False)
+    estado = db.Column(db.String(255), nullable=False)
+    cep = db.Column(db.String(255), nullable=False)
+    rua = db.Column(db.String(255), nullable=False)
+    numero = db.Column(db.String(255), nullable=False)
+    
+    complemento = db.Column(db.String(255), nullable=True)
+    
+    __table_args__ = (
+        db.Index('idx_cep', 'cep'),
+    )
+
+    def __repr__(self):
+        return f"<Endereco cidade={self.cidade} cep={self.cep}>"
