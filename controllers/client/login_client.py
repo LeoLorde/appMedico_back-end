4f03c1cd948fca2d8e98358443e0a674bb8f5faa
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-15 19:54:45 -0300

    feat: adding client login

---
+++ controllers/client/login_client.py
@@ -0,0 +1,27 @@
+from flask import request, jsonify
+from models.client_model import Client
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+
+def login():
+    data = request.get_json()
+    email=data.get('email')
+    password = data.get('password')
+
+    client_exist : Client = Client.query.filter_by(email=email).first()
+    if client_exist == None:
+        return jsonify({
+            "message":f"Usuário com email '{email}' não encontrado"
+        })
+    if client_exist.check_password(password) == False:
+        return jsonify({
+            "message":f"Senha incorreta"
+        })
+    
+    access_token = create_access_token(
+        identity=email,
+        expires_delta=timedelta(hours=1)
+    )
+    return jsonify({
+        'access_token': access_token
+    }), 200
