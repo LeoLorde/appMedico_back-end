fbd4586c932f63c433f4b28672fdd8ffcdee16b8
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-15 07:54:42 -0300

    feat: making user model abstract

--- models/user_model.py
+++ models/user_model.py
@@ -1,11 +1,17 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from database import db
+from abc import abstractmethod, ABC
+from datetime import datetime
 
-class User(db.Model):
+class User(ABC, db.Model):
+    __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(255), nullable=False)
     password_hash = db.Column(db.String(128))
+    
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -13,5 +19,6 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
+    @abstractmethod
     def __repr__(self):
         return f"<User | id: ${self.id} | username: ${self.username}"
