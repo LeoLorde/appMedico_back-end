f3d252fd50a0d350a3302c7ee0e6f19423d09bee
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-15 07:49:39 -0300

    feat: creating inital app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from database import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
