9ebc1c7ee3cb300a0e1b600aa1205af6113352d0
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-15 08:05:36 -0300

    feat: adding client user

---
+++ models/client_model.py
@@ -0,0 +1,26 @@
+from database import db
+from .user_model import User
+from enum import Enum
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Enum as sqlenum
+
+class Gender(Enum):
+    MAN = 1
+    WOMAN = 2
+
+from cryptography.fernet import Fernet
+
+class Client(User):
+    cpf = db.Column(db.String(128), nullable=False)
+    dataDeNasc = db.Column(db.DateTime, nullable=False)
+    gender = db.Column(sqlenum(Gender), nullable=False)
+    _key = b'NAO-EH-SEGURO-TIRA-ESSA-MERDA'
+
+    def set_cpf(self, cpf):
+        cipher = Fernet(self._key)
+        self.cpf = cipher.encrypt(cpf.encode()).decode()
+
+    def check_cpf(self, cpf):
+        cipher = Fernet(self._key)
+        decrypted_cpf = cipher.decrypt(self.cpf.encode()).decode()
+        return decrypted_cpf == cpf
