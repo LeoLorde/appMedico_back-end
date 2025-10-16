e6c6243b26b67b19707bbda3f85c0bada09bf4af
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-16 14:02:24 -0300

    feat: doctor email and crm validation

--- controllers/doctor/create_doctor.py
+++ controllers/doctor/create_doctor.py
@@ -1,21 +1,39 @@
 from models.doctor_model import Doctor
 from flask import request, jsonify
 from database import db
+from classes import Email, CRM
 
 def create_doctor():
     data = request.get_json()
-    doctor : Doctor = Doctor(
-        username=data.get('username'),
-        email=data.get('email'),
-        bio=data.get('bio'),
-        especialidade=data.get('especialidade'),
+
+    username = data.get('username')
+    email = data.get('email')
+    crm = data.get('crm')
+    bio = data.get('bio')
+    especialidade = data.get('especialidade')
+    senha = data.get('senha')
+
+    # ----- VALIDATORS -----
+    if not Email.is_valid(email):
+        return jsonify({'message': 'Email inválido'}), 400
+
+    if not CRM.is_valid(crm):
+        return jsonify({'message': 'CRM inválido'}), 400
+
+    # ----- CREATE DOCTOR -----
+    doctor = Doctor(
+        username=username,
+        email=Email.parse(email),
+        bio=bio,
+        especialidade=especialidade,
         endereco_id=1
     )
-    doctor.set_crm(crm=data.get('crm'))
-    doctor.set_password(password=data.get('senha'))
-    
+    doctor.set_crm(crm=CRM.parse(crm))
+    doctor.set_password(password=senha)
+
     db.session.add(doctor)
     db.session.commit()
+
     return jsonify({
         'message': 'Doctor criado com sucesso',
         'data': doctor.toMap()
