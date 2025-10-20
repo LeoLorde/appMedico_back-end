8c70b5f3508046baf86aa18a8cb0e35115793158
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-20 15:00:05 -0300

    feat: adding Expediente model

---
+++ models/expedient_model.py
@@ -0,0 +1,18 @@
+from database import db
+import uuid
+
+class Expediente(db.Model):
+    __tablename__ = 'expedientes'
+
+    id = db.Column(db.String(128), primary_key=True, default=uuid.uuid4)
+    horario_inicio = db.Column(db.Time, nullable=False)
+    horario_fim = db.Column(db.Time, nullable=False)
+    dias_trabalho = db.Column(db.ARRAY(db.String), nullable=False)  
+
+    def to_map(self):
+        return {
+            'id': str(self.id),
+            'horario_inicio': str(self.horario_inicio),
+            'horario_fim': str(self.horario_fim),
+            'dias_trabalho': self.dias_trabalho
+        }
