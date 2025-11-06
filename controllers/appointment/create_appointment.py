7056394fd58a631d6e3888fb7c2476e38efbe5d5
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-11-06 13:23:24 -0300

    refactor: refactoring create_appointment

--- controllers/appointment/create_appointment.py
+++ controllers/appointment/create_appointment.py
@@ -1,86 +1,20 @@
-from database import db
-from models.appointment_model import Appointment
-from models.client_model import Client
-from models.doctor_model import Doctor
-from models.fcm_token_model import FcmToken
 from flask import request, jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from utils.send_message import send_message
-from datetime import datetime, timedelta
+from flask_jwt_extended import jwt_required
+from create_appointment_logic import get_doctor_and_client_with_request, check_if_appointment_is_possible, create_appointment_model, send_notification_to_users
+from database import db
 
 @jwt_required()
 def create_appointment():
-    print("RECEIVED REQUEST")
     try:
-        data = request.get_json()
-        current_user = get_jwt_identity()
+        doctor, client = get_doctor_and_client_with_request(request.get_json())
         
-        validated_data = {
-            'doctor_id': data.get('doctor_id'),
-            'date': data.get('data_marcada', ''),
-            'time': data.get('time', '')
-        }
+        duration, start_time = check_if_appointment_is_possible(doctor, client, request.get_json()["data_marcada"])
+        appointment = create_appointment_model(start_time, client, doctor, request.get_json()["motivo"], duration)
         
-        client = Client.query.get(current_user)
-        if not client:
-            return jsonify({'message': 'Cliente não encontrado'}), 404
+        send_notification_to_users(client, doctor, appointment)
         
-        doctor = Doctor.query.get(validated_data['doctor_id'])
-        if not doctor:
-            return jsonify({'message': 'Médico não encontrado'}), 404
-        
-        duration_minutes = int(doctor.horario_min)
-        appointment_time = datetime.fromisoformat(data.get('data_marcada'))
-        appointment_time = appointment_time.replace(tzinfo=None)
-        end_time = appointment_time + timedelta(minutes=duration_minutes)
-
-        doctor_appointments = Appointment.query.filter(
-            Appointment.doctor_id == doctor.id
-        ).all()
-
-        for appt in doctor_appointments:
-            appt_start = appt.data_marcada
-            appt_end = appt_start + timedelta(minutes=appt.duration)
-            if appointment_time < appt_end and end_time > appt_start:
-                print("Não está dispnivel (medico)")
-                return jsonify({'message': 'O médico não está disponível neste horário.'}), 400
-
-        client_appointments = Appointment.query.filter(
-            Appointment.client_id == current_user
-        ).all()
-
-        for appt in client_appointments:
-            appt : Appointment = appt
-            appt_start = appt.data_marcada
-            appt_end = appt_start + timedelta(minutes=appt.duration)
-            if appointment_time < appt_end and end_time > appt_start:
-                return jsonify({'message': 'Você já tem um agendamento nesse horário.'}), 400
-        
-        
-        appointment = Appointment(
-            data_marcada=appointment_time,
-            client_id=current_user,
-            doctor_id=validated_data['doctor_id'],
-            is_confirmed="pending",
-            motivo=data.get("motivo"),
-            duration=duration_minutes
-        )
-        print("--------")
-        print(f"DATA RECEBIDA: {data.get("data_marcada")}")
-        print(f"DATA UTILIZADA: {appointment.data_marcada}")
-        print("--------")
         db.session.add(appointment)
         db.session.commit()
-        doctor : Doctor = doctor
-        
-        token : FcmToken = FcmToken.query.filter_by(user_id=client.id).first()
-        send_message(token.fcm_token, "Sua consulta foi enviada ao Doutor", "Consulta Enviada")
-        
-        token : FcmToken = FcmToken.query.filter_by(user_id=doctor.id).first()
-        send_message(token.fcm_token, f"Nova solicitação de consulta recebida: {appointment.motivo}", "Solicitação de Consulta")
-
-        print("DB URI:", db.engine.url)
-        print(f"Agendamento criado para {appointment_time} - {end_time}")
 
         return jsonify({
             'message': 'Agendamento criado com sucesso',
---
+++ controllers/appointment/create_appointment_logic.py
@@ -0,0 +1,76 @@
+from models.appointment_model import Appointment
+from models.client_model import Client
+from models.doctor_model import Doctor
+from models.fcm_token_model import FcmToken
+from models.user_model import User
+from flask import jsonify
+from flask_jwt_extended import get_jwt_identity
+from utils.send_message import send_message
+from datetime import datetime, timedelta
+
+def get_doctor_and_client_with_request(data) -> tuple[Doctor, Client]:
+    current_user = get_jwt_identity()
+    
+    validated_data = {
+        'doctor_id': data.get('doctor_id'),
+        'date': data.get('data_marcada', ''),
+        'time': data.get('time', '')
+    }
+    
+    client = Client.query.get(current_user)
+    if not client:
+        raise ValueError("Cliente não existe")
+    
+    doctor = Doctor.query.get(validated_data['doctor_id'])
+    if not doctor:
+        raise ValueError("Doutor não existe")
+    
+    return doctor, client
+
+def calculate_end_time(data_marcada: str, duracao_minutos: int) -> datetime:
+    inicio = datetime.fromisoformat(data_marcada).replace(tzinfo=None)
+    fim = inicio + timedelta(minutes=duracao_minutos)
+    return fim
+
+def format_appointment_date(data_marcada: str):
+    return datetime.fromisoformat(data_marcada).replace(tzinfo=None)
+
+def check_user_datetime_is_free(user : User, appointment_time, end_time):
+    user_appointments = Appointment.query.filter(
+            Appointment.user_id == user.id
+        ).all()
+
+    for appointment in user_appointments:
+        appointment_start = appointment.data_marcada
+        appointment_end = appointment_start + timedelta(minutes=appointment.duration)
+        if appointment_time < appointment_end and end_time > appointment_start:
+            print("Não está dispnivel (medico)")
+            return jsonify({'message': 'O médico não está disponível neste horário.'}), 400
+
+def send_notification_to_users(user1: User, user2: User, appointment: Appointment):
+        token : FcmToken = FcmToken.query.filter_by(user_id=user1.id).first()
+        send_message(token.fcm_token, "Sua consulta foi enviada ao Doutor", "Consulta Enviada")
+        
+        token : FcmToken = FcmToken.query.filter_by(user_id=user2.id).first()
+        send_message(token.fcm_token, f"Nova solicitação de consulta recebida: {appointment.motivo}", "Solicitação de Consulta")
+
+def check_if_appointment_is_possible(doctor, client, data_marcada):
+        duration_minutes = int(doctor.horario_min)
+        end_time = calculate_end_time(data_marcada, duration_minutes)
+
+        appointment_time = format_appointment_date(data_marcada)
+
+        check_user_datetime_is_free(doctor, appointment_time, end_time)
+        check_user_datetime_is_free(client, appointment_time, end_time)
+        
+        return duration_minutes, end_time, appointment_time
+        
+def create_appointment_model(appointment_time, client, doctor, motivo, duration):
+        return Appointment(
+            data_marcada=appointment_time,
+            client_id=client.id,
+            doctor_id=doctor.id,
+            is_confirmed="pending",
+            motivo=motivo,
+            duration=duration
+        )
